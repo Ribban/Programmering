8386d9174c7db3b52d9a6e65944913589f4d04eb
Ribban <doppel2ganger@gmail.com>
2019-09-12 11:57:48 +0200

    Har fortsatt med arbetet

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -9,20 +9,53 @@ namespace Bank
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Välkommen till banken!");
-            Console.WriteLine("Ange vilket av följande alternativ du önskar göra.");
-            Console.WriteLine("1 : Lägg till en användare");
-            Console.WriteLine("2 : Ta bort en användare");
-            Console.WriteLine("3 : Visa alla befintliga användare");
-            Console.WriteLine("4 : Visa saldo för en användare");
-            Console.WriteLine("5 : Gör en insättning för en användare");
-            Console.WriteLine("6 : Gör ett uttag för en användare");
-            Console.WriteLine("7 : Avsluta programmet");
-            Console.WriteLine(" ");
-            Console.Write("Skriv ditt val:");
-            int choise = int.Parse(Console.ReadLine());
+            bool notDone = true;
+            while (notDone == true) {
+                Console.WriteLine("Välkommen till banken!");
+                Console.WriteLine("Ange vilket av följande alternativ du önskar göra.");
+                Console.WriteLine("1 : Lägg till en användare");
+                Console.WriteLine("2 : Ta bort en användare");
+                Console.WriteLine("3 : Visa alla befintliga användare");
+                Console.WriteLine("4 : Visa saldo för en användare");
+                Console.WriteLine("5 : Gör en insättning för en användare");
+                Console.WriteLine("6 : Gör ett uttag för en användare");
+                Console.WriteLine("7 : Avsluta programmet");
+                Console.WriteLine(" ");
+                Console.Write("Skriv ditt val:");
+                int choice = int.Parse(Console.ReadLine());
 
-            switch(choise)
+                switch (choice)
+                {
+                    case 1:
+                        Customer customer = new Customer();
+                        Console.Write("Ange ditt namn: ");
+                        customer.name = Console.ReadLine();
+                        Console.Write("Ange ditt nuvarande saldo: ");
+                        customer.balance = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Välkommen till vår bank " + customer.name);
+                        customerList.Add(customer);
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+                        foreach (var array in customerList)
+                        {
+                            Console.WriteLine();
+                        }
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        notDone = false;
+                        break;
+                }
+            
+            }
         }
     }
 }
