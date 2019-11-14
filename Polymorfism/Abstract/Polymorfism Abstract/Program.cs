6bbee1296cbc20a970ac2daba6877f418badf2c0
Ribban <doppel2ganger@gmail.com>
2019-11-14 11:33:24 +0100

    Har gjort ett exempel på abstract och på virtual

---
+++ Polymorfism/Abstract/Polymorfism Abstract/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorfism_Abstract
+{
+    class Supa
+    {
+        public void Dricka()
+        {
+            Console.WriteLine("Dags att supa");
+        }
+    }
+    class Hotellet : Supa
+    {
+        public void Dricka()
+        {
+            Console.WriteLine("Dags att supa på hotellet");
+        }
+    }
+    class Hemma : Supa
+    {
+        public void Dricka()
+        {
+            Console.WriteLine("Dags att supa hemma");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Supa> alkolister = new List<Supa>();
+
+            alkolister.Add(new Supa());
+            alkolister.Add(new Hotellet());
+            alkolister.Add(new Hemma());
+            foreach (Supa s in alkolister)
+            {
+                s.Dricka();
+            }
+        }
+    }
+}
