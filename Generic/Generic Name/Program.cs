2fbdb30d3d10985299eb74f097e6d5fc89c76f9e
Ribban <doppel2ganger@gmail.com>
2020-01-16 11:44:14 +0100

    Klar med första uppgiften, börjat med andra

--- Generic/Generic Name/Program.cs
+++ Generic/Generic Name/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,21 @@ using System.Threading.Tasks;
 
 namespace Generic_Name
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataSample<string> dss = new DataSample<string>("Forty-two");
+            DataSample<int> dsi = new DataSample<int>(42);
+            String s = dss.GetData();
+            int i = dsi.GetData();
+            Console.WriteLine(String.Format("s={0}, i={1}", s, i));
+            Console.ReadKey();
+        }
+    }
     public class DataSample<T>
     {
+        
         private T theData;
         public DataSample(T theData)
         {
@@ -25,11 +39,6 @@ namespace Generic_Name
         {
             T element = arr[arr.Length / 2];
             return element;
-        }
-        static void Main(string[] args)
-        {
-            String[] words = { "C++", "MacOS", "C#", "Android", "Python" };
-            String middle = DataSample<string>.getMiddleElement(words);
         }
     }
 }
---
+++ Generic/Generic Project Two/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Generic_Project_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                public class ListOfObjects<T>
+        {
+            private T[] theList;
+            private int noOfElements = 10;
+            private int nextIndex;
+
+            public ListOfObjects()
+            {
+                theList = new T[noOfElements];
+                nextIndex = 0;
+            }
+            public void AddObject(T o)
+            {
+
+            }
+            public T GetObject(int index)
+            {
+
+            }
+        }
+    }
+    }
+}
