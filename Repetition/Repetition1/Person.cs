fd675c8aecae0e4d05955bcaf9bd83e1cd60464d
Ribban <doppel2ganger@gmail.com>
2020-01-29 12:57:32 +0100

    Börjat på Swimmer uppgiften

---
+++ Repetition/Repetition1/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repetition1
+{
+    class Person
+    {
+        private string Förnamn;
+        private string Efternamn;
+        public string GetFullName()
+        {
+            return Förnamn + Efternamn;
+        }
+    }
+}
---
+++ Simhall/Uppgift 1 - Simmare/Swimmer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_1___Simmare
+{
+    class Swimmer
+    {
+        private string name;
+        private BathingSuit suit;
+        public string swim;
+    }
+    class BathingSuit
+    {
+
+    }
+    class ProfessionalSwimmer
+    {
+
+    }
+    class YoungSwimmer
+    {
+        private bool tired;
+    }
+}
