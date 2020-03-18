0256674ff89c5a03060fdfdfb05328c487c721c3
Ribban <doppel2ganger@gmail.com>
2020-03-18 13:00:26 +0100

    Har fortsatt med tictactoe

--- TicTacToe/TicTacToe/Form1.cs
+++ TicTacToe/TicTacToe/Form1.cs
@@ -94,9 +94,27 @@ namespace TicTacToe
         }
         Winner GetWinner()
         {
+            PictureBox[] winningMoves = {
+                                             a1, a2, a3,
+                                             b1, b2, b3,
+                                             c1, c2, c3,
+                                             a1, b1, c1,
+                                             a2, b2, c2,
+                                             a3, b3, c3,
+                                             a1, b2, c3,
+                                             a3, b2, c1
+                                          };
+            for (int i = 0; i < winningMoves.Length; i += 3)
+            {
+
+            }
 
         }
         void ShowTurn()
+        {
+
+        }
+        private void OnClick(object sender, EventArgs e)
         {
 
         }
