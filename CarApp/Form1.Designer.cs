d227a1d19f2de943db99ead06a1c34e1612c3d76
Ribban <doppel2ganger@gmail.com>
2020-04-02 11:55:13 +0200

    CarApp grejen

---
+++ CarApp/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace CarApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.cbxForSale = new System.Windows.Forms.CheckBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtMake = new System.Windows.Forms.TextBox();
+            this.txtRegNr = new System.Windows.Forms.TextBox();
+            this.lsvCars = new System.Windows.Forms.ListView();
+            this.RegNr = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.make = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.model = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.year = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.sale = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.cbxForSale);
+            this.groupBox1.Controls.Add(this.txtYear);
+            this.groupBox1.Controls.Add(this.txtModel);
+            this.groupBox1.Controls.Add(this.txtMake);
+            this.groupBox1.Controls.Add(this.txtRegNr);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(711, 153);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(592, 81);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(89, 37);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Lägg till";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // cbxForSale
+            // 
+            this.cbxForSale.AutoSize = true;
+            this.cbxForSale.Location = new System.Drawing.Point(592, 26);
+            this.cbxForSale.Name = "cbxForSale";
+            this.cbxForSale.Size = new System.Drawing.Size(89, 24);
+            this.cbxForSale.TabIndex = 5;
+            this.cbxForSale.Text = "Till Salu";
+            this.cbxForSale.UseVisualStyleBackColor = true;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(361, 86);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(180, 26);
+            this.txtYear.TabIndex = 4;
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(361, 20);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(180, 26);
+            this.txtModel.TabIndex = 3;
+            // 
+            // txtMake
+            // 
+            this.txtMake.Location = new System.Drawing.Point(86, 86);
+            this.txtMake.Name = "txtMake";
+            this.txtMake.Size = new System.Drawing.Size(180, 26);
+            this.txtMake.TabIndex = 2;
+            // 
+            // txtRegNr
+            // 
+            this.txtRegNr.Location = new System.Drawing.Point(86, 23);
+            this.txtRegNr.Name = "txtRegNr";
+            this.txtRegNr.Size = new System.Drawing.Size(180, 26);
+            this.txtRegNr.TabIndex = 1;
+            // 
+            // lsvCars
+            // 
+            this.lsvCars.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.RegNr,
+            this.make,
+            this.model,
+            this.year,
+            this.sale});
+            this.lsvCars.HideSelection = false;
+            this.lsvCars.Location = new System.Drawing.Point(12, 191);
+            this.lsvCars.Name = "lsvCars";
+            this.lsvCars.Size = new System.Drawing.Size(711, 205);
+            this.lsvCars.TabIndex = 1;
+            this.lsvCars.UseCompatibleStateImageBehavior = false;
+            this.lsvCars.View = System.Windows.Forms.View.Details;
+            this.lsvCars.SelectedIndexChanged += new System.EventHandler(this.lsvCars_SelectedIndexChanged);
+            // 
+            // RegNr
+            // 
+            this.RegNr.Text = "Reg Nr";
+            // 
+            // make
+            // 
+            this.make.Text = "Märke";
+            // 
+            // model
+            // 
+            this.model.Text = "Model";
+            // 
+            // year
+            // 
+            this.year.DisplayIndex = 4;
+            this.year.Text = "År";
+            // 
+            // sale
+            // 
+            this.sale.DisplayIndex = 3;
+            this.sale.Text = "Till Salu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Reg Nr";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Märke";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(295, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Model";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(295, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "År";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 408);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(215, 36);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Rensa all data";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(504, 408);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(219, 36);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Ta bort markerad";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 450);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lsvCars);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView lsvCars;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.CheckBox cbxForSale;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtMake;
+        private System.Windows.Forms.TextBox txtRegNr;
+        private System.Windows.Forms.ColumnHeader RegNr;
+        private System.Windows.Forms.ColumnHeader make;
+        private System.Windows.Forms.ColumnHeader model;
+        private System.Windows.Forms.ColumnHeader year;
+        private System.Windows.Forms.ColumnHeader sale;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnRemove;
+    }
+}
+
---
+++ CarApp/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtRegNr.Focus();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtRegNr.Text) || string.IsNullOrEmpty(txtMake.Text)) 
+            {
+                MessageBox.Show("Du måste fylla i alla rutor", "Felaktig inmatning");
+            } else
+            {
+                ListViewItem item = CreateListViewItem(txtRegNr.Text, txtMake.Text, txtModel.Text, txtYear.Text,cbxForSale.Checked);
+                lsvCars.Items.Add(item);
+                ClearTextboxes();
+                btnClear.Enabled = true;
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lsvCars.SelectedItems.Count > 0)
+            {
+                var item = lsvCars.SelectedItems[0];
+                lsvCars.Items.Remove(item);
+                MessageBox.Show("bilen med registeringsnummer " + item.Text + " är borttagen", "Borttag av bil");
+            }
+            else
+            {
+                MessageBox.Show("Ingen bil var markerad att tas bort", "Borttag av bil");
+            }
+            btnClear.Enabled = (lsvCars.Items.Count > 0);
+        }
+
+        private void lsvCars_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnRemove.Enabled = (lsvCars.SelectedItems.Count > 0);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lsvCars.Clear();
+        }
+        private ListViewItem CreateListViewItem(string regNr, string make, string model, string year, bool forSale)
+        {
+            ListViewItem item = new ListViewItem(regNr);
+            item.SubItems.Add(make);
+            item.SubItems.Add(forSale ? "Yes" : "No");
+            item.SubItems.Add(model);
+            item.SubItems.Add(year);
+            return item;
+        }
+        private void ClearTextboxes()
+        {
+            txtRegNr.Clear();
+            txtMake.Clear();
+            txtModel.Clear();
+            txtYear.Clear();
+            cbxForSale.Checked = false;
+            txtRegNr.Focus();
+        }
+    }
+}
