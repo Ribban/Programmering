d060add1fbfd862d27e99f647535ba835790352b
Ribban <doppel2ganger@gmail.com>
2020-04-22 12:52:19 +0200

    posh

---
+++ CarApp/Database.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApp
+{
+    class Database
+    {
+        public SqliteConnection dbConn;
+    }
+}
