8dcfafdb51198464de070763e8edca321a106aed
Ribban <44964120+Ribban@users.noreply.github.com>
2020-06-03 12:59:36 +0200

    Klar med allt

--- Chatapp-Egen/Form1.cs
+++ Chatapp-Egen/Form1.cs
@@ -37,13 +37,20 @@ namespace Chatapp_Egen
         {
             
         }
-
+        /// <summary>
+        /// Funktion för att ta emot meddelanden
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void receive_Click(object sender, EventArgs e)
         {
-            //
+            //Skapar ny instans av IPEndPoint med ipadress och portnummer
             IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            //Binder UdpClient till portnummret 12345
             UdpClient client = new UdpClient(12345);
+            //Tar emot meddelandet
             byte[] instream = client.Receive(ref clientEndPoint);
+            //Visar meddelandet i rutan
             messageTextBox.Text = Encoding.Unicode.GetString(instream);
         }
     }
--- Chatapp-client/Form1.cs
+++ Chatapp-client/Form1.cs
@@ -14,14 +14,22 @@ namespace Chatapp_client
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Skickar ett meddelande över nätverket
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void send_Click(object sender, EventArgs e)
         {
-            //
+            //Tar emot meddelandet som ska skickas
             byte[] message = Encoding.Unicode.GetBytes(messageTextBox.Text);
+            //Converterar en sträng till ipadress
             IPAddress serverIP = IPAddress.Parse("127.0.0.1");
+            //Skapar en ny instans av IPEndPoint med ipadress och portnummer
             IPEndPoint destination = new IPEndPoint(serverIP, 12345);
+            //Initierar en ny instans av UdpClient
             UdpClient client = new UdpClient();
+            //Tar meddelandet och skickar det över nätverket
             client.Send(message, message.Length, destination);
         }
     }
